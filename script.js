439072389d7483b3ffc8431ee783725eb8f5280c
Nooman Miled <noon.supply@gmail.com>
2022-11-06 19:00:06 +0100

    commit modification urls

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('http://localhost:3000/year')
+fetch('https://whatyearisit-frontend-lake.vercel.app')
 .then(response => response.json())
 .then(data => {
   document.querySelector('#year').textContent = data.value;
